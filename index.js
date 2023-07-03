da3b60f0d2d28f5d847a7377a0e993b380516cb9
Ruiming Tang <v-ruitang@microsoft.com>
2023-07-03 17:10:59 +0800

    Init version for a express server uploaded.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
+app.post('/audio/:audioId', (req, res) => {
+  const fileName = req?.params?.audioId;
+  if (fileName) {
+    try {
+      const file = fs.readFileSync(`resource/${fileName}`);
+      res.send(file);
+    } catch (error) {
+      res.status(500).send({
+        error,
+      })
+    }
+    
+  } else {
+    res.status(400).send({
+      error: "Audio id is invalid",
+    });
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
